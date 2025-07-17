65f441b6f572adf12e6354955cde0c637d987d64
jovankoledin <jovank@uw.edu>
2025-07-16 20:14:11 -0700

    Update fft.c

--- benchmarks/fft.c
+++ benchmarks/fft.c
@@ -46,3 +46,34 @@ void fft(double real[FFT_SIZE], double img[FFT_SIZE], double real_twid[FFT_SIZE/
         }
     }
 }
+
+int main(){
+    double data_x[size];
+    double data_y[size];
+    double img[size];
+    double real[size];
+    int i;
+
+    //set up twiddles...
+    double twoPI = 6.28318530717959;
+    double typed;
+    int n, N;
+    N = size;
+
+    //Pre-calc twiddles
+    for(n=0; n<(N>>1); n++){
+        typed = (double)(twoPI*n/N);
+        real[n] = cos(typed);
+        img[n] = (-1.0)*sin(typed);
+    }
+
+    //Init data
+    for(i=0; i < size; i++) {
+        data_x[i] = (double)(i);
+        data_y[i] = (double)(i);
+    }
+
+    fft(data_x, data_y, real, img);
+
+    return 0;
+} 
